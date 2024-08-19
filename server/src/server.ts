a649838cb8e21d230c35250e76dcdc943c6871b4
surya-situ <suryasocial55@gmail.com>
2024-08-19 15:12:12 +0530

    feat: sending testing mail to the user

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,26 @@
 import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
+import ejs from "ejs";
+import { sendEmail } from "./config/mail";
 
 dotenv.config();
 
 const app:Application = express();
 const PORT = process.env.PORT || 7000;
 
-app.get('/', (req: Request, res: Response) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// View Engine
+app.set("view engine", "ejs")
+app.set("views", path.resolve(__dirname, "./views"));
+
+app.get('/', async (req: Request, res: Response) => {
+    const html = await ejs.renderFile(__dirname + `/views/emails/welcome.ejs`, {name: "surya"});
+    await sendEmail('iamkantasurya@gmail.com', 'Testing', html)
     return res.status(200).json({
-        status: "Ok",
-        message: "Hello from server"
+        message: 'email send successfully'
     });
 });
 

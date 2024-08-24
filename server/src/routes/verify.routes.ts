ebc303a6b434618c08c2ff0afa80099daae9a8c0
surya-situ <suryasocial55@gmail.com>
2024-08-24 22:14:52 +0530

    feat: user email verificatin route

---
+++ server/src/routes/verify.routes.ts
@@ -0,0 +1,41 @@
+import { Router, Request, Response} from "express";
+
+import prisma from "../config/database";
+
+const router = Router();
+
+router.get("/verify-email", async(req: Request, res: Response) => {
+    const { email, emailVerifyToken } = req.query;
+
+    if( email && emailVerifyToken ) {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email as string
+            }
+        });
+
+        if(user) {
+            if(emailVerifyToken === user.email_verified_token) {
+                await prisma.user.update({
+                    where:{
+                        email: email as string
+                    },
+                    data: {
+                        email_verified_token: null,
+                        email_verified_at: new Date().toISOString()
+                    }
+                })
+
+                return res.redirect(`${process.env.CLIENT_APP_URL}/login`)
+            }
+        }
+
+        return res.redirect("/verify-error");
+    }
+});
+
+router.get("/verify-error", async(req: Request, res: Response) => {
+    res.render("../views/auth/emailVerifyerror.ejs");
+});
+
+export default router

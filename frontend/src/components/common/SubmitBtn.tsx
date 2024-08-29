e7542b1229b459a579fc06d09584bcdb2730ae02
surya-situ <suryasocial55@gmail.com>
2024-08-29 20:05:13 +0530

    feat: submit button for registration and dynamically showing wheather processing or idel

---
+++ frontend/src/components/common/SubmitBtn.tsx
@@ -0,0 +1,14 @@
+import { useFormStatus } from "react-dom";
+import { Button } from "../ui/button";
+
+export function SubmitBtn() {
+    const { pending } = useFormStatus();
+
+    return (
+        <Button className="w-full" disabled={pending}>
+            {
+                pending ? "Processing..." : "Submit"
+            }
+        </Button>
+    )
+}

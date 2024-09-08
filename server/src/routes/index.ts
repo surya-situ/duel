f84277665b23fdf89f32d85e44ea31bb7a42281b
surya-situ <suryasocial55@gmail.com>
2024-09-08 19:02:36 +0530

    feat: index file for the routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import AuthRoute from "./auth.routes";
-import VerifyRoute from "./verify.routes"
+import VerifyRoute from "./verify.routes";
+import PasswordRoute from "./password.routes"
 
 const router = Router()
 
-router.use("/api/auth", AuthRoute);
-router.use("/", VerifyRoute);
+router.use("/api/auth", AuthRoute); // - user crud routes
+router.use("/api/auth", PasswordRoute); // - user password reset route
+router.use("/", VerifyRoute); // - user verify route
 
 export default router;

6eee3331410a6ae1b27fb904113489daa98b5b80
surya-situ <suryasocial55@gmail.com>
2024-09-10 11:51:23 +0530

    feat: secure Duel routes with authentication middleware and add Password routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import AuthRoute from "./auth.routes";
 import VerifyRoute from "./verify.routes";
-import PasswordRoute from "./password.routes"
+import PasswordRoute from "./password.routes";
+import DuelRoute from "./duel.routes";
+import authMiddleware from "../middleware/authMiddleware";
 
 const router = Router()
 
+// - user router
 router.use("/api/auth", AuthRoute); // - user crud routes
 router.use("/api/auth", PasswordRoute); // - user password reset route
 router.use("/", VerifyRoute); // - user verify route
+
+// - duel router *Private routes
+router.use("/api/duel", authMiddleware, DuelRoute)
 
 export default router;

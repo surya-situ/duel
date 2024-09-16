59ed1560afdd879463bdf2327f0091f11ba0c28c
surya-situ <suryasocial55@gmail.com>
2024-09-16 11:05:45 +0530

    docs: typescript config file

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      boxShadow: {
+        'bottom': '0 4px 6px rgba(0, 0, 0, 0.1)',
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",

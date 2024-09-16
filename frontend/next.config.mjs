29526de5e470c080c33534d2133f77b603076f33
surya-situ <suryasocial55@gmail.com>
2024-09-16 11:10:37 +0530

    config: added the host and protocol for socket.io

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "localhost",
+                protocol: "http"
+            },
+            {
+                hostname: "*",
+                protocol: "https"
+            }
+        ]
+    },
+    reactStrictMode: false
+};
 
 export default nextConfig;

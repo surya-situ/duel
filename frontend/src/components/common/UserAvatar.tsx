542944bf71fa71f7d0b6498d11ab1bd9337c771f
surya-situ <suryasocial55@gmail.com>
2024-09-16 11:47:15 +0530

    feat: common componet (submitbtn, avartar) for form and user avartar

---
+++ frontend/src/components/common/UserAvatar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default function UserAvatar() {
+    return (
+        <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>duel</AvatarFallback>
+        </Avatar>
+                
+    )
+};

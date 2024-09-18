24e204c1ea12d2d909f5900c045d5100e70aa8dd
surya-situ <suryasocial55@gmail.com>
2024-09-18 14:51:53 +0530

    feat: implement login page with session check and redirect to dashboard if logged in

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,33 +1,33 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 
-const login = () =>{
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import Login from "@/components/auth/Login";
+import { redirect } from "next/navigation";
+
+const login = async () =>{
+
+    // check session for user logIn
+    const session = await getServerSession(authOptions);
+
+    // - redirect to dashboard page if the user is already loggedIn
+    if(session) {
+        redirect("/dashboard")
+    }
+
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="w-[550px] bg-white rounded-xl px-10 py-5 shadow-md">
-                <h1 className="text-center text-4xl font-extrabold bg-gradient-to-r from-blue-500 to-blue-900 text-transparent bg-clip-text">Duel</h1>
+                <h1 className="text-center text-4xl font-extrabold bg-gradient-to-r from-blue-500 to-blue-900 text-transparent bg-clip-text">
+                    {/* <Link href="/"> */}
+                        Duel
+                    {/* </Link> */}
+                </h1>
                 <div className="text-2xl font-bold mt-2">Login</div>
                 <p>Welcome back</p>
 
-                <form>
-                    <div className="mt-4">
-                        <Label htmlFor="email">Email</Label>
-                        <Input id="email" type="email" name="email" placeholder="Enter email address" />
-                        <Label htmlFor="password">Password</Label>
-                        <Input id="password" type="password" name="password" placeholder="Enter password" />
-
-                        <div className="text-right font-semibold mt-3">
-                            <Link href="forget password">Forget password ?</Link>
-                        </div>
-                       
-                    </div>
-
-                    <div className="mt-4">
-                        <Button className="w-full">Submit</Button>
-                    </div>
-                </form>
+                {/* - Login component to perform login action using client side */}
+                <Login />
 
                 <div className="mt-3">
                     Don't have an account ? <strong className="font-semibold"><Link href="register">Register</Link></strong> 

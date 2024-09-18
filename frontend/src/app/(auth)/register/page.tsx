3960a78005ab2c5d2283761a5360ed327e2afb70
surya-situ <suryasocial55@gmail.com>
2024-09-18 14:53:09 +0530

    feat: added the register component to register and show toast

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,6 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import Register from "@/components/auth/Register";
 import Link from "next/link";
 
 const register = () =>{
@@ -11,25 +9,8 @@ const register = () =>{
                 <div className="text-2xl font-bold mt-2">Register</div>
                 <p>Hi there!</p>
 
-                <form>
-                    <div className="mt-4">
-                        <Label htmlFor="name">Name</Label>
-                        <Input id="name" type="name" name="name" placeholder="John Doe" />
-
-                        <Label htmlFor="email">Email</Label>
-                        <Input id="email" type="email" name="email" placeholder="Enter email address" />
-
-                        <Label htmlFor="password">Password</Label>
-                        <Input id="password" type="password" name="password" placeholder="Enter password" />
-                       
-                        <Label htmlFor="cpassword">Confirm password</Label>
-                        <Input id="cpassword" type="password" name="confirm_password" placeholder="Enter confirm password" />
-                    </div>
-
-                    <div className="mt-4">
-                        <Button className="w-full">Submit</Button>
-                    </div>
-                </form>
+                {/* - Form register to show toaster in the client side - */}
+                <Register />
 
                 <div className="mt-3">
                     Already have an account ? <strong className="font-semibold"><Link href="login">login</Link></strong> 
